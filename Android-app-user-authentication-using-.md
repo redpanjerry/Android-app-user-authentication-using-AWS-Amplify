9c79d107d65e3214a02b2f52caee1d6ccb973079
王允辰 <jerryw@JerryMacBook-Pro.local>
2021-04-11 04:45:10 -0700

    First commit, working

---
+++ Android-app-user-authentication-using-.md
@@ -0,0 +1,10 @@
+# Android-app-user-authentication-using-AWS-Amplify
+
+## Prerequisites
+- An AWS account
+- Android Studio
+- AWS Cli installed
+
+# Step-by-step
+
+Working...
---
+++ Authentication/app/src/main/java/com/example/jerry_lab1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.jerry_lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
